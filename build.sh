af7779add7b08165d8cde012e3c004881141bf6c
jdowning <programmervsworld@gmail.com>
2026-05-30 20:43:34 -0400

    Made build script update a zip of all contents for deploy

--- build.sh
+++ build.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
-rm -f sample.bas
-rm -f sprites.bin
+rm -f build/bwgolf.bas
+rm -f build/sprites.bin
 
 cat src/game.bas | python3 number.py > build/bwgolf.bas
 
 cd sprites
 ./update2.sh
 
+cd ../backgrounds
+python3 png2raw.py fnxversionp2f.png background 320 240
+mv background.bin ../build/
+mv background.pal ../build/
+
 cd ../build/
-sudo python3 fnxmgr.zip --copy sprites.bin
-read -n 1 -s -p "Press any key to continue..."
 
-sudo python3 fnxmgr.zip --copy background.pal
-read -n 1 -s -p "Press any key to continue..."
+zip game.zip background.bin background.pal bwgolf.bas sprites.bin
 
-sudo python3 fnxmgr.zip --copy background.bin
-read -n 1 -s -p "Press any key to continue..."
+#sudo python3 fnxmgr.zip --copy sprites.bin
+#read -n 1 -s -p "Press any key to continue..."
+
+#sudo python3 fnxmgr.zip --copy background.pal
+#read -n 1 -s -p "Press any key to continue..."
+
+#sudo python3 fnxmgr.zip --copy background.bin
+#read -n 1 -s -p "Press any key to continue..."
 
 sudo python3 fnxmgr.zip --binary bwgolf.bas --address 28000
