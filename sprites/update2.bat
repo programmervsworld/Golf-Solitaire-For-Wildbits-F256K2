31c7f3291d499c421b7e92ce61fa46aeb012c985
jdowning <programmervsworld@gmail.com>
2026-06-14 17:14:11 -0400

    Removing unused powershell and bat files

--- sprites/update2.bat
+++
@@ -1,5 +0,0 @@
-REM spritebuild.py is the x1 core way, spritebuilder2.py needs to be used on x2 cores otherwise
-REM the output will be shifted off by one byte
-REM python spritebuild.py sprites.def
-python spritebuild2.py sprites.def
-move graphics.bin ../sprites.bin
